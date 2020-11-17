5730dae8b5aebc18ca69e281cc4b9d7568b425b5
sarami <sara.unimail@gmail.com>
2020-11-17 21:02:51 +0100

    Updated fixed mistakes

--- main.java
+++ main.java
@@ -2,10 +2,11 @@ public class myAwesomeClass {
 
   public static void  main(String [] args) {
     int myVariable = 15;
-    for (int j == 1; j <= 20; j++) {
+    for (int j = 1; j <= 20; j++) {
       myVariable = myVariable * j;
       System.out.println(j);
       System.out.println(" ");
       System.out.println(myVariable);
     }
+  }
 }
