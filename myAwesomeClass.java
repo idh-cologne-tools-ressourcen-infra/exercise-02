8668b10175005bf138e9cce1dc6afa86b4ba43b1
Moritz Ulrich <moritz@tarn-vedra.de>
2020-11-19 10:37:51 +0100

    myVariable: Use `long` to prevent numeric overflow

--- myAwesomeClass.java
+++ myAwesomeClass.java
@@ -1,7 +1,7 @@
 public class myAwesomeClass {
 
   public static void main(String[] args) {
-    int myVariable = 15;
+    long myVariable = 15;
     for (int j = 1; j < 20; j++) {
       myVariable = myVariable * j;
       System.out.println(j + "\t" + myVariable);
