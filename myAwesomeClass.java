f467916f4a88ded7663a2a77e9b0bf1823894a43
Moritz Ulrich <moritz@tarn-vedra.de>
2020-11-19 10:36:59 +0100

    Use tabular output

--- myAwesomeClass.java
+++ myAwesomeClass.java
@@ -4,9 +4,7 @@ public class myAwesomeClass {
     int myVariable = 15;
     for (int j = 1; j < 20; j++) {
       myVariable = myVariable * j;
-      System.out.print(j);
-      System.out.println(" ");
-      System.out.println(myVariable);
+      System.out.println(j + "\t" + myVariable);
     }
   }
 }
